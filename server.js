d39306bbdee250c6844d1d1c78a3771efcc00f16
skay97 <salmankarimzindani@hotmail.com>
2019-03-23 12:25:06 -0500

    update

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
-// Dependencies
-// =============================================================
+require("dotenv").config();
 var express = require("express");
+var exphbs = require("express-handlebars");
 
-// Sets up the Express App
-// =============================================================
+var db = require("./models");
+
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+// Middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// Static directory to be served
-app.use(express.static("app/public"));
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 // Routes
-// =============================================================
-require("./app/routes/api-routes.js")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// Here we introduce HTML routing to serve different HTML files
-require("./app/routes/html-routes.js")(app);
+var syncOptions = { force: false };
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
+
+module.exports = app;
