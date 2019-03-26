5ea59cc4bf1fa1094b2b15ec668dff15d4f4864d
skay97 <salmankarimzindani@hotmail.com>
2019-03-25 19:53:29 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var sequelize = require("sequelize");
-
 var db = require("./models");
 
 var app = express();
