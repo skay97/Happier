ead9147ea1654d0cd89a1701dbcaf7ea55d9664b
hliang <hliang@github>
2019-03-27 10:00:13 -0500

    fix routes and handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,21 +4,30 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.happyhour.findAll({}).then(function(dbResult) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbResult
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+  app.get("/enter", function(req, res) {
+    db.happyhour.findAll({}).then(function(dbResult) {
+      res.render("enter", {
+        msg: "Welcome to enter page!",
+        hhEntries: dbResult
+      });
+    });
+  });
+
+  // view happy hours
+  app.get("/view", function(req, res) {
+    db.happyhour.findAll({}).then(function(dbResult) {
+      res.render("view", {
+        msg: "Welcome to view page!",
+        hhEntries: dbResult
       });
     });
   });
